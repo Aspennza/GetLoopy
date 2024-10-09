3ba6babe9c0172308ae3b72c2d64241a356f13aa
Zoe Aspenns <aspennza@mail.uc.edu>
2024-10-09 09:46:36 -0400

    Task 7 draft complete

--- src/Main.java
+++ src/Main.java
@@ -41,3 +41,16 @@ class DownByTwo
         }
     }
 }
+
+class SquareFigure
+{
+    public static void main(String[] args)
+    {
+        for(int row = 1; row <= 5; row++) {
+            for(int col = 1; col <= 5; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
