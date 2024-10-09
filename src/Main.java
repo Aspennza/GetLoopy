5060e62de7df42fe0789d7b27f1adefcf0327141
Zoe Aspenns <aspennza@mail.uc.edu>
2024-10-09 12:07:25 -0400

    Tasks 6 and 8 drafts complete

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
@@ -67,5 +70,63 @@ class TriangleIncreasing
             }
             System.out.println();
         }
+    }
+}
+
+class TriangleDecreasing
+{
+    public static void main(String[] args)
+    {
+        for(int row = 5; row >= 1; row--)
+        {
+            for(int col = 1; col <= row; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class DieRoller
+{
+    public static void main(String[] args)
+    {
+        Random gen = new Random();
+        Scanner in = new Scanner(System.in);
+        int die1 = 0;
+        int die2 = 1;
+        int die3 = 2;
+        int dieSum = 0;
+        int rollNum = 0;
+        String userContinue = "";
+        boolean done = false;
+
+        do {
+            System.out.println("\nRoll    Die1    Die2    Die3    Sum");
+            System.out.println("--------------------------------------");
+            die1 = 0;
+            die2 = 1;
+            die3 = 2;
+
+            while (die1 != die2 || die2 != die3) {
+                   rollNum++;
+
+                   die1 = gen.nextInt(6) + 1;
+                   die2 = gen.nextInt(6) + 1;
+                   die3 = gen.nextInt(6) + 1;
+
+                   dieSum = die1 + die2 + die3;
+
+                   System.out.printf("%4d %7d %7d %7d %6d", rollNum, die1, die2, die3, dieSum);
+                   System.out.println();
+            }
+            System.out.print("\nWould you like to play again? Enter Q to quit or press any other key to continue: ");
+            userContinue = in.nextLine();
+
+            if(userContinue.equalsIgnoreCase("Q")) {
+                done = true;
+            }
+        }while(!done);
     }
 }
