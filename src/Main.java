82629621b988767757815a985eafdcdfa5ce1332
Zoe Aspenns <aspennza@mail.uc.edu>
2024-10-09 09:33:14 -0400

    Tasks 1 and 2 complete

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        for(int c = 0; c <= 30; c++)
+        {
+            System.out.print("" + c + " ");
+        }
+    }
+}
+
+class Decrementing
+{
+    public static void main(String[] args)
+    {
+        for(int n = 30; n >= 0; n--)
+        {
+            System.out.print("" + n + " ");
+        }
     }
 }
